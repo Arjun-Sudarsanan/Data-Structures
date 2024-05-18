60fd82c2931adfcb997f40b4a4b4c48d6d090780
Arjun-Sudarsanan <71219837+Arjun-Sudarsanan@users.noreply.github.com>
2024-05-18 14:45:23 +0530

    Add files via upload

---
+++ DSA/array.js
@@ -0,0 +1,56 @@
+class NewArray {
+
+    constructor() {
+        this.data = {};
+        this.length = 0;
+    }
+
+    getItemAt(index) {
+        return this.data[index];
+    }
+
+    getArrayItems() {
+        return this.data;
+    }
+
+    pushItem(item) {
+        this.data[this.length] = item;
+        this.length++;
+    }
+
+    popItem() {
+        const deletedItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return deletedItem;
+    }
+
+    deleteItemAt(index) {
+        for (let i = index; i < this.length; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length -1];
+        this.length--;
+        return this.data[this.length];
+
+    }
+
+}
+
+const myNewArray = new NewArray();
+myNewArray.pushItem('Hi');
+myNewArray.pushItem('My');
+myNewArray.pushItem('Name');
+myNewArray.pushItem('Is');
+myNewArray.pushItem('Arjun');
+console.log('....', myNewArray.getArrayItems());
+
+console.log('....', myNewArray.getItemAt(2));
+
+myNewArray.popItem();
+console.log('....', myNewArray.getArrayItems());
+
+myNewArray.deleteItemAt(2);
+console.log('....', myNewArray.getArrayItems());
+
+
