bea675b2b790f99eeb5adbf61f7635f2342b46f5
Arjun-Sudarsanan <71219837+Arjun-Sudarsanan@users.noreply.github.com>
2024-10-27 17:35:12 -1200

    Create linked-list-implementation.js
    Implemented basic operations of linked list like: append, prepend, insert at position

---
+++ DSA/linked-list-implementation.js
@@ -0,0 +1,66 @@
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            val: value,
+            next: null
+        }
+        this.tail = this.head;
+        this.count = 1;
+    }
+
+    append(newValue) {
+        this.tail.next = {
+            val: newValue,
+            next: null
+        };
+        this.tail = this.tail.next;
+        this.length++;
+        return this.head;
+    }
+    
+    prepend(newValue) {
+        const tempReference = this.head;
+        this.head = {
+            val: newValue,
+            next: tempReference
+        };
+        this.length++;
+        return this.head;
+    }
+
+    addAtPos(pos, newValue) {
+        if (pos < 0 || pos > this.length) {
+            console.warn("Not a valid index");
+        }
+        else if (pos === 0) {
+            this.prepend(newValue);
+        }
+        else if (pos === this.length) {
+            this.append(newValue);
+        }
+        else {
+            const foundValue = this.findElement(pos - 1);
+            const newNode = {
+                val: newValue,
+                next: foundValue.next
+            };
+            foundValue.next = newNode;
+            this.length++;
+        }
+    }
+
+    findElement(pos) {
+        let ele = this.head;
+        for (let i = 0; i < pos; i++) {
+            ele = ele.next;
+        }
+        return ele;
+    }
+}
+
+const newLinkedList = new LinkedList(5);
+newLinkedList.append(50);
+newLinkedList.append(100);
+newLinkedList.prepend(30);
+newLinkedList.addAtPos(2, 300);
